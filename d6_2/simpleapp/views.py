c4de7212130597b72f26de8684a2fc27fe888acf
Slava-inc <54527208+Slava-inc@users.noreply.github.com>
2024-05-16 11:24:37 +0300

    filter added renew

--- d6_2/simpleapp/views.py
+++ d6_2/simpleapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
 from datetime import datetime
+from .filters import ProductFilter
 
 
 class ProductsList(ListView):
@@ -19,6 +20,19 @@ class ProductsList(ListView):
     context_object_name = 'products'
     paginate_by = 2  # вот так мы можем указать количество записей на странице
 
+   # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+   
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
     def get_context_data(self, **kwargs):
@@ -28,10 +42,14 @@ class ProductsList(ListView):
         # В ответе мы должны получить словарь.
         context = super().get_context_data(**kwargs)
         # К словарю добавим текущую дату в ключ 'time_now'.
-        context['time_now'] = datetime.utcnow()
-        # Добавим ещё одну пустую переменную,
-        # чтобы на её примере рассмотреть работу ещё одного фильтра.
-        context['next_sale'] = None
+        # context['time_now'] = datetime.utcnow()
+        # # Добавим ещё одну пустую переменную,
+        # # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        # context['next_sale'] = None
+        
+        # Добавляем в контекст объект фильтрации.
+        context['filterset'] = self.filterset 
+               
         return context
 
 class ProductDetail(DetailView):
