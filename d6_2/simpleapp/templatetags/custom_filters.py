e468cba5ec5e604ca1cb54f4825e458347e248f8
slavaincos <slavaincos@gmail.com>
2024-05-19 10:29:18 +0300

    parameters query coding

--- d6_2/simpleapp/templatetags/custom_filters.py
+++ d6_2/simpleapp/templatetags/custom_filters.py
@@ -18,3 +18,11 @@ def currency(value, code='rub'):
    postfix = CURRENCIES_SYMBOLS[code]
    # Возвращаемое функцией значение подставится в шаблон.
    return f'{value} {postfix}'
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+   d = context['request'].GET.copy()
+   for k, v in kwargs.items():
+       d[k] = v
+   return d.urlencode()
+
